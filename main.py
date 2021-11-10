839ab8f9c7cfe68c2fb834c0ad9ca6a158cfe77f
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-10 17:19:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def create_person(title, author, genre, year_of_publish, no_of_pages):
     my_book.no_of_pages = no_of_pages
     return my_book
 
-my_book = create_book('Friends', 'John Smith', 'comedy', '1993', '1570')
 
 my_book = Book()
 my_book.__dict__['title'] = 'Friends'
@@ -23,9 +22,24 @@ my_book.__dict__['year_of_publish'] = '1993'
 my_book.__dict__['no_of_pages'] = 1570
 
 my_book = Book()
-my_book.title = title
-my_book.author = author
-my_book.genre = genre
-my_book.year_of_publish = year_of_publish
-my_book.no_of_pages = no_of_pages
+my_book.title = 'Friends'
+my_book.author = 'Jonh Smith'
+my_book.genre = 'comedy'
+my_book.year_of_publish = 1993
+my_book.no_of_pages = 1570
+
+
+
+***** targil 4:
   
+  
+class Car:
+    pass
+
+my_car = Car
+my_car.mark = 'Ford'
+my_car.model = 'Mustang'
+my_car.year = 1999
+my_car.engine = 2500
+my_car.color = 'grey'
+print(my_car)  
